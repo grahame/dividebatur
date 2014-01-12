6192b2b1afba190fce0d5253b54f6286b15ca852
grahame@angrygoats.net <grahame@angrygoats.net>
2014-01-13 03:00:22 +0800

    break out regexp

--- angular/js/filters.js
+++ angular/js/filters.js
@@ -1,7 +1,8 @@
 angular.module('divideBaturFilters', []).filter('numberfmt', function() {
+  var r = /\B(?=(\d{3})+(?!\d))/g;
   return function(input) {
     // http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
-    return (input || "").toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return (input || "").toString().replace(r, ",");
   };
 }).filter('f2', function() {
     return function(input) {
