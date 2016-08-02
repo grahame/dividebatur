6a6df2498bc57dbd877e407fc6c8e64091892c72
Grahame Bowland <grahame@angrygoats.net>
2016-08-02 13:38:05 +0800

    travis tests: only run counts with verification

--- travis_tests.sh
+++ travis_tests.sh
@@ -9,9 +9,7 @@ nosetests dividebatur
 
 flake8 --ignore=E501 dividebatur
 
-echo "** Tests: Federal 2013 WA **"
-./run_aec_fed2013_wa.sh
-
-echo "** Tests: Federal 2016 **"
-./run_aec_fed2016.sh
+for i in aec_data/*/verified.json; do
+    time python3 -m dividebatur.senatecount "$i" './angular/data/'
+done
 
