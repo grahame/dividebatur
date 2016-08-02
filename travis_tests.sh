c9eae36c99072aeb283a0d7a873a987ff93f0eba
Grahame Bowland <grahame@angrygoats.net>
2016-08-02 23:57:57 +0800

    travis tests: run the linter last, so we can tell if the important tests are running

--- travis_tests.sh
+++ travis_tests.sh
@@ -7,9 +7,8 @@ pip3 install nose
 
 nosetests -v dividebatur
 
-flake8 --ignore=E501 dividebatur
-
 for i in aec_data/*/verified.json; do
     time python3 -m dividebatur.senatecount "$i" './angular/data/'
 done
 
+flake8 --ignore=E501 dividebatur
