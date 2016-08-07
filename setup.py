c02eee8e5db5e70546a66c2d03e1970df8ac0394
Grahame Bowland <grahame@angrygoats.net>
2016-08-07 23:27:07 +0800

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     keywords="stv senate voting",
     url="https://github.com/grahame/dividebatur",
     name="dividebatur",
-    version="0.7.0",
+    version="0.8.0",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     extras_require={
         'dev': dev_requires
