ba031243b5e710a1ad6b39441a3bbfd6c0a06f85
Grahame Bowland <grahame@angrygoats.net>
2016-08-10 02:08:36 +0800

    fix profiling

--- profile/prof.py
+++ profile/prof.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     from dividebatur import senatecount
     import cProfile
     import sys
-    cProfile.run('senatecount.execute_counts(*sys.argv[2:])', filename=sys.argv[1])
+    cProfile.run('senatecount.execute_counts(sys.argv[2], sys.argv[3], None, None)', filename=sys.argv[1])
