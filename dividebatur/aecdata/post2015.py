c511f332fbad1af099180e1c10e01337e2f87b1c
Grahame Bowland <grahame@angrygoats.net>
2016-08-09 23:15:35 +0800

    roughly 50% performance improvement in read of FormalPrefences

--- dividebatur/aecdata/post2015.py
+++ dividebatur/aecdata/post2015.py
@@ -1,22 +1,30 @@
 import csv
-import re
 from collections import Counter
-
-from .utils import int_or_none
 
 
 class FormalPreferences:
     "parse AEC 'formal preferences' CSV file"
 
     def __init__(self, csv_file):
+        """
+        csv_file: preferences file to read
+
+        candidate_count: used to build a fast lookup table for the text
+        representation of preferences, and defines the highest possible
+        preference that can be cast above or below the line
+        """
         self._csv_file = csv_file
 
     def __iter__(self):
-        sub_re = re.compile('\*|/')
-
-        def parse_prefs(s):
-            ", delimited, with * and / meaning 1"
-            return tuple(int_or_none(t) for t in sub_re.sub('1', s).split(','))
+        # this is a bit of a hack: it'll blow up if someone numbers a box
+        # >= 1024, and the AEC enter that data in. however, it fails harmlessly
+        # with an Exception, rather than introducing incorrect data into the
+        # count, and it's twice as fast as other ways I've come up with to
+        # write this code.
+        pref_hash = dict((str(i), i) for i in range(1, 1024))
+        pref_hash['*'] = 1
+        pref_hash['/'] = 1
+        pref_hash[''] = None
         with open(self._csv_file, 'rt') as fd:
             reader = csv.reader(fd)
             header = next(reader)
@@ -25,4 +33,4 @@ class FormalPreferences:
             assert(dummy == ['------------', '---------------------', '---------------------', '-------', '-------', '-----------'])
             form_count = Counter(t[5] for t in reader)
             for form, count in form_count.items():
-                yield parse_prefs(form), count
+                yield tuple(pref_hash[t] for t in form.split(',')), count
